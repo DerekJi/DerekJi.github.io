6332dd7cfb094932e13eb80423ccce10772c928d
Derek Ji <33775203+DerekJi@users.noreply.github.com>
2020-09-25 09:40:34 +0930

    [Fri, Sep 25, 2020  9:22:58 AM] Deployed from ng-bt-resume@gh-pages
    DON'T REMOVE /bin/deploy.sh

---
+++ bin/deploy.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+REPOS=ng-bt-resume
+BRANCH=gh-pages
+ROOT_DIR=$PWD
+
+# PREPARE
+ls | grep -v bin | xargs rm -rf
+
+# Clone & checkout
+URL="https://github.com/DerekJi/$REPOS.git"
+git clone $URL
+cd $REPOS
+git checkout $BRANCH
+
+# Copy
+cp -rf ./* .. -rf
+cd ..
+rm -rf $REPOS
+
+# Commit
+now=`date`
+git add -A
+git commit -m "[$now] Deployed from $REPOS@$BRANCH"
+
+# Push to remote
+echo "Please manually run 'git push' to submit the changes"
